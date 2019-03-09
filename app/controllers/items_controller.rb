15bd1ebb98b29edd7229426359fd2b0330a5a532
Nakamura <gireru@hotmail.co.jp>
2019-03-09 17:51:21 +0900

    削除機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,11 @@ class ItemsController < ApplicationController
   def edit 
     @item = Item.find(params[:id])
   end
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
 
   def update
    @item = Item.find(params[:id])
